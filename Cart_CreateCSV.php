c983128a6292dd9c1049012cfd35ba14db680b35
Mr.Blinky <mstr.blinky@gmail.com>
2022-04-23 22:13:49 +0200

    update Cart_CreateCSV.php, fix csv, fix image
    added PHP code to create flash image bin file
    fixed file extension of Silence text adventure hex file
    replaced Ninja Fuzzgrawth title screen

--- Cart_CreateCSV.php
+++ Cart_CreateCSV.php
@@ -1,5 +1,229 @@
 <?php
 
+    #CSV indices
+    const ID_LIST = 0;
+    const ID_TITLE = 1;
+    const ID_TITLESCREEN = 2;
+    const ID_HEXFILE = 3;
+    const ID_DATAFILE = 4;
+    const ID_SAVEFILE = 5;
+    const ID_VERSION = 6;
+    const ID_DEVELOPER = 7;
+    const ID_INFO = 8;
+    const ID_LIKES = 9;
+    const ID_MAX = 10;
+    
+    function FixPath($filename)
+    {
+      return str_replace("\\", "/", $filename);
+    }
+    
+    function DefaultHeader()
+    {
+      return "ARDUBOY" . str_repeat(chr(0xFF),249);
+    }
+    
+    function LoadTitleScreenData($filename)
+    {
+      if (!is_file($filename)) return false;
+    
+      $tmp = @imageCreateFromPng($filename);
+      $width  = @imagesx($tmp);
+      $height = @imagesy($tmp);
+      $img = imagecreatetruecolor(128,64);
+      @imagecopyresampled($img, $tmp, 0, 0, 0, 0, 128, 64, $width, $height);
+      imageDestroy($tmp);
+      $bytes = str_repeat(chr(0xFF), 1024);
+      $i = 0;
+      $b = 0;
+      for ($y = 0; $y < 64; $y += 8 )
+      {
+        for ($x = 0; $x < 128; $x++)
+        {
+          for ($p = 0; $p < 8; $p++)
+          {
+             $b = $b >> 1;
+             if (imagecolorat($img, $x, $y + $p) > 0) $b = $b | 0x80;
+          }
+          $bytes[$i] = chr($b);
+          $i++;
+        }
+      }
+      imageDestroy($img);
+      return $bytes;
+    }
+    
+    function LoadHexFileData($filename)
+    {
+      if (!is_file($filename)) return false;
+      if ($file = fopen($filename, "r"))
+      {
+        $bytes = str_repeat(chr(0xFF), 29696);
+        $flash_end = 0;
+        while(!feof($file))
+        {
+          $rcd = fgets($file);
+          if ($rcd == ":00000001FF") break;
+          if ($rcd[0] == ":")
+          {
+            $rcd_len  = hexdec(substr($rcd, 1, 2));
+            $rcd_typ  = hexdec(substr($rcd, 7, 2));
+            $rcd_addr = hexdec(substr($rcd, 3, 4));
+            $rcd_sum  = hexdec(substr($rcd, 9 + $rcd_len * 2, 2));
+            if (($rcd_typ == 0) && ($rcd_len > 0))
+            {
+              $flash_addr = $rcd_addr;
+              $checksum = $rcd_sum;
+              for ($i = 1; $i < 9 + $rcd_len * 2; $i += 2)
+              {
+                  $byte = hexdec(substr($rcd, $i, 2));
+                  $checksum = ($checksum + $byte) & 0xFF;
+                  if ($i >= 9)
+                  {
+                    $bytes[$flash_addr] = chr($byte);
+                    $flash_addr ++;
+                    if ($flash_addr > $flash_end) $flash_end = $flash_addr;
+                  }
+              }
+              if ($checksum != 0) return false;
+            }
+          }
+        }
+        fclose($file);
+        $flash_end = ($flash_end + 255) & 0xFF00;
+        $bytes = substr($bytes, 0, $flash_end);
+        return $bytes;
+      }
+      else return false;
+    }
+    
+    function LoadDataFile($filename)
+    {
+      if (!is_file($filename)) return false;
+      $bytes = file_get_contents($filename);
+      if (is_string($bytes))
+      {
+        return $bytes . str_repeat(chr(0xFF), 256 - strlen($bytes) % 256);
+      }
+      return false;
+    }
+    
+    function LoadSaveFile($filename)
+    {
+      if (!is_file($filename)) return false;
+      $bytes = file_get_contents($filename);
+      if (is_string($bytes))
+      {
+        return $bytes . str_repeat(chr(0xFF), 4096 - strlen($bytes) % 4096);
+      }
+      return false;
+    }
+    
+    #function CreateFlashImage($filename)
+    function CreateFlashImage($csv)
+    {
+      #if (!is_file($filename)) return false;
+      #if ($file = fopen($filename, "r"))
+      {
+        $binfile = "";
+        $previouspage = 0xFFFF;
+        $currentpage = 0;
+        $nextpage = 0;
+        $sep = ';';
+        #$head = fgets($file);
+        $head = current($csv);
+        if (substr_count($head, ',') > substr_count($head, ';')) $sep = ',';
+        #while(!feof($file))
+        while (next($csv) !== false)
+        {
+          #$row = fgetcsv($file, 0, $sep);
+          $row = str_getcsv(current($csv), $sep);
+          if (count($row) == 1 && $row[ID_LIST] == "") continue; #ignore blank lines
+          while (count($row) < 10) $row[] = "";
+          $header = DefaultHeader();
+          $title = LoadTitleScreenData(FixPath($row[ID_TITLESCREEN]));
+          if (strlen($title) != 1024)
+          {
+            #fclose($file);
+            echo var_dump($row);
+            return false;
+          }
+          $program = LoadHexFileData(FixPath($row[ID_HEXFILE]));
+          $programsize = strlen($program);
+          $datafile = LoadDataFile(FixPath($row[ID_DATAFILE]));
+          $datasize = strlen($datafile);
+          $savefile = LoadSaveFile(FixPath($row[ID_SAVEFILE]));
+          $savesize = strlen($savefile);
+          $id = hash("sha256" , $program . $datafile, true);
+          $programpage = $currentpage + 5;
+          $datapage    = $programpage + ($programsize >> 8);
+          $alignpage   = $datapage + ($datasize >> 8);
+          if ($savesize > 0) $alignsize = (16 - $alignpage % 16) * 256;
+          else $alignsize = 0;
+          $slotsize = (($programsize + $datasize + $alignsize + $savesize) >> 8) + 5;
+          $savepage    = $alignpage + ($alignsize >> 8);
+          $nextpage += $slotsize;
+          $header[7]  = chr((int)$row[ID_LIST]); #list number
+          $header[8]  = chr($previouspage >> 8);
+          $header[9]  = chr($previouspage & 0xFF);
+          $header[10] = chr($nextpage >> 8);
+          $header[11] = chr($nextpage & 0xFF);
+          $header[12] = chr($slotsize >> 8);
+          $header[13] = chr($slotsize & 0xFF);
+          if (substr($program, -128) == str_repeat(chr(0xFF), 128)) $header[14] = chr(($programsize >> 7) - 1); #no need to flash unused 128 byte page
+          else $header[14] = chr($programsize >> 7); #program size in 128 byte pages
+          if ($programsize > 0)
+          {
+            $header[15] = chr($programpage >> 8);
+            $header[16] = chr($programpage & 0xFF);
+            if ($datasize > 0)
+            {
+              $program[0x14] = chr(0x18);
+              $program[0x15] = chr(0x95);
+              $program[0x16] = chr($datapage >> 8);
+              $program[0x17] = chr($datapage & 0xFF);
+              $header[17] = chr($datapage >> 8);
+              $header[18] = chr($datapage & 0xFF);
+            }
+            if ($savesize > 0)
+            {
+              $program[0x18] = chr(0x18);
+              $program[0x19] = chr(0x95);
+              $program[0x1a] = chr($savepage >> 8);
+              $program[0x1b] = chr($savepage & 0xFF);
+              $header[19] = chr($savepage >> 8);
+              $header[20] = chr($savepage & 0xFF);
+            }
+            for ($i = 0; $i < strlen($id); $i++) $header[25 + $i] = $id[$i];
+            $stringdata = $row[ID_TITLE] . chr(0) . $row[ID_VERSION] . chr(0) . $row[ID_DEVELOPER] . chr(0) . $row[ID_INFO] . chr(0);
+          }
+          else
+          {
+            $stringdata = $row[ID_TITLE] . chr(0) . $row[ID_INFO] . chr(0);
+          }
+          if (strlen($stringdata) > 199) $stringdata = substr($stringdata,0,199);
+          for ($i = 0; $i < strlen($stringdata); $i++) $header[57 + $i] = $stringdata[$i];
+          $binfile .= $header;
+          $binfile .= $title;
+          $binfile .= $program;
+          $binfile .= $datafile;
+          $binfile .= str_repeat(chr(0xFF), $alignsize);
+          $binfile .= $savefile;
+          $previouspage = $currentpage;
+          $currentpage = $nextpage;
+        }
+        #fclose($file);
+        $binfile .= str_repeat(chr(0xFF), 256); #use blank header to signal end of FX file system
+        $filename = "flashcart-image.bin";
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'. $filename .'"');
+        header('Content-Length: ' . strlen($binfile));
+        echo $binfile;
+        return true;
+      }
+    }
+
     // Create a header image with the supplied text ..
 
     function createHeaderImage($string, $fileName, $fontSize = 15) {
@@ -48,14 +272,13 @@
 
     }
 
-    // if ($_POST["mode"] == "bin") {
-
-    //     // $command = escapeshellcmd('python3 flashcart-builder.py newfile.csv');
-    //     // $output = shell_exec($command);
-
-    //     // echo $output;
-    //     header( "Content-type: image/png" );
-    // }
+    // Create flash image file from CSV data
+    
+    if ($_POST["mode"] == "bin") {
+        set_time_limit(0);
+        $csv = explode("<eol/>", $_POST["output"]);
+        if (!CreateFlashImage($csv)) echo "Error creating flash image.";
+    }
 
 
     // Create a header image using the post field 'categroryName' ..
